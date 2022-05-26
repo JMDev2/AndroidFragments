22e714ce46cccf360aea9e700290b8060f34cc23
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-26 08:11:49 +0300

    This is the initial commit

---
+++ app/src/main/java/com/moringaschool/fragments/MoodDialogFragment.java
@@ -0,0 +1,64 @@
+package com.moringaschool.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class MoodDialogFragment extends DialogFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+
+
+        //declaring the member variables for the buttons
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+
+        //Returning values from the from
+
+        //1.pulling the entire radiogroup
+        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);
+        //2.get the selected ID
+        int selectedId = surveyRadioGroup.getCheckedRadioButtonId();
+        //retireve the value
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+
+        //submit the data
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+
+
+
+
+            }
+
+
+        });
+        return rootView;
+
+    }
+}
+
+
+
